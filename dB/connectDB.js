8e306239d8ad2fd7edcee51938bcc05cc056055a
SURAJ SINGH <84866441+surajsingh78271@users.noreply.github.com>
2023-10-19 09:39:41 +0530

    Update connectDB.js

--- dB/connectDB.js
+++ dB/connectDB.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose")
 const connectDB = async (DATABASE_URL)=>{
     try{
         return await mongoose.connect(DATABASE_URL,{
-            dbName:"clientTemplate1"
+            dbName:"clientTemplate1",
+            useNewUrlParser: true,
+        useUnifiedTopology: true,
            }).then(()=>{
                 console.log("DB CONNECT SUCCESFULLY.")
             })
